9ef3900a97afb92609ecdd1629a196c8147f723d
Liam <phuc01112002@gmail.com>
2024-01-26 10:52:28 +0700

    sign token jwt and hash password

--- src/services/users.services.ts
+++ src/services/users.services.ts
@@ -1,17 +1,54 @@
 import { RegisterRequestBody } from '~/models/requests/User.requests'
 import database from './database.services'
-import { User } from '~/models/schemas/Users.schema'
+import { User } from '~/models/schemas/User.schema'
+import { hashPassword } from '~/utils/crypto'
+import { signToken } from '~/utils/jwt'
+import { TokenType } from '~/constants/enum'
+import { config } from 'dotenv'
+import { RefreshToken } from '~/models/schemas/RefreshToken.schema'
+import { ObjectId } from 'mongodb'
+config()
 
 class UserService {
+  private signAccessToken(user_id: string) {
+    return signToken({
+      payload: { user_id, token_type: TokenType.AccessToken },
+      options: { expiresIn: process.env.ACCESS_TOKEN_EXPIRES_IN }
+    })
+  }
+
+  private signRefreshToken(user_id: string) {
+    return signToken({
+      payload: { user_id, token_type: TokenType.RefreshToken },
+      options: { expiresIn: process.env.REFRESH_TOKEN_EXPIRES_IN }
+    })
+  }
+
+  private signAccessAndRefreshToken(user_id: string) {
+    return Promise.all([this.signAccessToken(user_id), this.signRefreshToken(user_id)])
+  }
   async register(payload: RegisterRequestBody) {
     const result = await database.user.insertOne(
       new User({
         ...payload,
+        password: hashPassword(payload.password),
         date_of_birth: new Date(payload.date_of_birth)
       })
     )
+    const user_id = result.insertedId.toString()
+    const [access_token, refresh_token] = await this.signAccessAndRefreshToken(user_id)
 
-    return result
+    await database.refreshToken.insertOne(
+      new RefreshToken({
+        user_id: new ObjectId(user_id),
+        token: refresh_token
+      })
+    )
+
+    return {
+      access_token,
+      refresh_token
+    }
   }
 
   async checkEmailExists(email: string) {
