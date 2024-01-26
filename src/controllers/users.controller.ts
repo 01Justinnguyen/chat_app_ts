f8b0ee3eee6721f42027c0b7eec24ad351ebea1a
Liam <phuc01112002@gmail.com>
2024-01-26 22:38:34 +0700

    create wrapRequestHandler

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -1,17 +1,13 @@
-import { Request, Response } from 'express'
+import { NextFunction, Request, Response } from 'express'
 import { ParamsDictionary } from 'express-serve-static-core'
 import { CLIENT_MESSAGE } from '~/constants/messages'
 import { RegisterRequestBody } from '~/models/requests/User.requests'
 import userService from '~/services/users.services'
 
 export const registerController = async (req: Request<ParamsDictionary, any, RegisterRequestBody>, res: Response) => {
-  try {
-    const result = await userService.register(req.body)
-    return res.json({
-      message: CLIENT_MESSAGE.REGISTER_SUCCESS,
-      result
-    })
-  } catch (error) {
-    return res.json(error)
-  }
+  const result = await userService.register(req.body)
+  return res.json({
+    message: CLIENT_MESSAGE.REGISTER_SUCCESS,
+    result
+  })
 }
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,9 +1,10 @@
 import { Request, Response, Router } from 'express'
 import { registerController } from '~/controllers/users.controller'
 import { registerMiddleware } from '~/middlewares/users.middleware'
+import { wrapRequestHandler } from '~/utils/wrapRequestHandler'
 
 const usersRouter = Router()
 
-usersRouter.post('/register', registerMiddleware, registerController)
+usersRouter.post('/register', registerMiddleware, wrapRequestHandler(registerController))
 
 export default usersRouter
