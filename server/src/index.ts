137a9c2427bd51a7e3f35bd7afa17f18b2d36292
Liam <phuc01112002@gmail.com>
2024-02-25 22:11:47 +0700

    coding feature upload single image by formidable

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,9 +4,14 @@ import cors from 'cors'
 import usersRouter from './routes/users.routes'
 import database from './services/database.services'
 import { defaultErrorsHandler } from './middlewares/errors.middleware'
+import mediaRoutes from './routes/media.routes'
+import { initFolderUploads } from './utils/file'
 config()
 const app = express()
 const PORT = process.env.PORT || 8888
+
+//init folder uploads
+initFolderUploads()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
@@ -20,6 +25,7 @@ app.use(
 database.connect()
 
 app.use('/users', usersRouter)
+app.use('/medias', mediaRoutes)
 
 app.use(defaultErrorsHandler)
 
